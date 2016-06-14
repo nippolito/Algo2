d63e522c63eb35d542610ce73d7d40f7abf89477
Ignacio Manuel Fernandez <ifernandez@ws11.labo5>
2016-06-14 15:52:52 -0300

    Anda dato

--- TP3/tp3-base/Modulos/Dato.h
+++ TP3/tp3-base/Modulos/Dato.h
@@ -12,36 +12,90 @@ namespace modulos {
 
 class Dato {
 		public:
-			 Dato();
-			 Dato DatoString(String s);
-			 Dato DatoNat(Nat n);
-			 bool Nat() const;
-			 bool String() const;
+			// Dato();
+			 Dato(String s);
+			 Dato(Nat n);
+			 bool EsNat() const;
+			 bool EsString() const;
 			 bool MismoTipo(const Dato otro) const;
 			 modulos::Nat ValorNat() const;
 			 modulos::String ValorStr() const;
 			 Dato Max(const Conj<Dato> cd);
 			 Dato Min(const Conj<Dato> cd);
-			 bool MenoroIgual(const Dato otro) const;
+			 bool MenorOIgual(const Dato otro) const;
 			 
 		private:
 			bool tipo;
 			modulos::Nat nat;
 			modulos::String str;			
 };
-
-	 Dato::Dato(): tipo(true) , nat(0) , str(" ") {}
+	//Dato::Dato(): tipo(true) , nat(0) , str(" ") {}
 	 
-	 Dato Dato::DatoString(String s){
-	 	Dato res = Dato();
-	 	res.tipo = false;
-	 	res.nat = 0;
-	 	res.str = s;
-	 	return res;
-	 }
+	Dato::Dato(String s): tipo(false) , nat(0) , str(s) {} 
+
+	Dato::Dato(Nat n): tipo(true) , nat(n) , str(" ") {} 
 	
+	bool Dato::EsNat() const {
+		return tipo;
+	}
 
-	 
+	bool Dato::EsString() const {
+		return !tipo;
+	}
+
+	bool Dato::MismoTipo(const Dato otro) const{
+		return (tipo == otro.tipo);
+	}
+ 
+ 	modulos::Nat Dato::ValorNat() const{
+ 		return nat;
+ 	}
+
+ 	modulos::String Dato::ValorStr() const{
+ 		return str;
+ 	}
+
+ 	bool Dato::MenorOIgual(const Dato otro) const{
+ 		bool res;
+ 			if(tipo){
+    		res = (nat <= otro.nat);
+			}else{
+			int i = 0;
+			while(i < str.size() && i < otro.str.size() && str[i] == otro.str[i]){
+				i++;
+			}
+			if(i == str.size() || i == otro.str.size()){
+			res = (str.size() <= otro.str.size() ); 
+			}else{
+			res = (str[i] <= otro.str[i] ) ;
+			}
+			}
+	}
+
+
+ 	Dato Dato::Max(const Conj<Dato> cd){
+ 		typename Conj<Dato>::const_Iterador i = cd.CrearIt();
+		Dato res = i.Siguiente();
+		while(i.HaySiguiente()){
+			if(!(i.Siguiente().MenorOIgual(res))){
+				res = i.Siguiente();
+			}
+			i.Avanzar();
+ 		}
+ 	return res;
+	}
+
+	Dato Dato::Min(const Conj<Dato> cd){
+ 		typename Conj<Dato>::const_Iterador i = cd.CrearIt();
+		Dato res = i.Siguiente();
+		while(i.HaySiguiente()){
+			if((i.Siguiente().MenorOIgual(res))){
+				res = i.Siguiente();
+			}
+			i.Avanzar();
+ 		}
+ 	return res;
+	}
 
 
 };
