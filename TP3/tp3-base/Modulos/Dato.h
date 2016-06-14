a30609b5e437d5e15cc1e8a0be276b9c8d464a6f
Ignacio Manuel Fernandez <ifernandez@ws11.labo5>
2016-06-14 14:32:01 -0300

    me fijo

--- TP3/tp3-base/Modulos/Dato.h
+++ TP3/tp3-base/Modulos/Dato.h
@@ -5,39 +5,45 @@
 #include <string>
 #include <cassert>
 #include "ModulosBasicos.h"
+
 using namespace aed2;
 
 namespace modulos {
+
 class Dato {
 		public:
-			 DatoString(String s);
-			 DatoNat(Nat n);
-			 bool Nat?() const;
-			 Nat ValorNat()
+			 Dato();
+			 Dato DatoString(String s);
+			 Dato DatoNat(Nat n);
+			 bool Nat() const;
+			 bool String() const;
+			 bool MismoTipo(const Dato otro) const;
+			 modulos::Nat ValorNat() const;
+			 modulos::String ValorStr() const;
+			 Dato Max(const Conj<Dato> cd);
+			 Dato Min(const Conj<Dato> cd);
+			 bool MenoroIgual(const Dato otro) const;
+			 
+		private:
+			bool tipo;
+			modulos::Nat nat;
+			modulos::String str;			
 };
 
+	 Dato::Dato(): tipo(true) , nat(0) , str(" ") {}
+	 
+	 Dato Dato::DatoString(String s){
+	 	Dato res = Dato();
+	 	res.tipo = false;
+	 	res.nat = 0;
+	 	res.str = s;
+	 	return res;
+	 }
+	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	 
 
 
 };
 
+#endif
