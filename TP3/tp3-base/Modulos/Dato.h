3ef764831be659e6ad7dc5132ed4fa737e9c3965
Ignacio Manuel Fernandez <ifernandez@ws18.labo7>
2016-06-14 12:34:05 -0300

    agregue carpeta modulos

---
+++ TP3/tp3-base/Modulos/Dato.h
@@ -0,0 +1,43 @@
+#ifndef MODULOS_DATO_H_INCLUDED   //dato es parte de la carpeta modulos
+#define MODULOS_DATO_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <cassert>
+#include "ModulosBasicos.h"
+using namespace aed2;
+
+namespace modulos {
+class Dato {
+		public:
+			 DatoString(String s);
+			 DatoNat(Nat n);
+			 bool Nat?() const;
+			 Nat ValorNat()
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
