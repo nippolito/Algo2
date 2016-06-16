cce1d31ada3be59a25a18ebbb477346ae0b1c726
Ignacio Manuel Fernandez <ifernandez@ws6.labo5>
2016-06-16 13:36:30 -0300

    ahora funciona dato y arranque con dicclog

--- TP3/tp3-base/Modulos/Dato.h
+++ TP3/tp3-base/Modulos/Dato.h
@@ -14,7 +14,7 @@ namespace modulos {
 
 class Dato {
 		public:
-			// Dato();
+			 Dato();
 			 ~Dato();
 			 Dato(String s);
 			 Dato(Nat n);
@@ -38,120 +38,9 @@ class Dato {
 			modulos::Nat nat;
 			modulos::String str;			
 };
-	//Dato::Dato(): tipo(true) , nat(0) , str(" ") {}
-	 
-	Dato::Dato(String s): tipo(false) , nat(0) , str(s) {} 
-
-	Dato::Dato(Nat n): tipo(true) , nat(n) , str(" ") {} 
-	
-	Dato::Dato(const Dato& otro){
-		tipo = otro.tipo;
-		nat = otro.nat;
-		str = otro.str;
-	}
-	
-	Dato::~Dato(){} // Las clases que tienen miembros estaticos (cosas sin punteros) la funcion ~Dato destruye la tupla automaticamente
-
-
-	bool Dato::EsNat() const {
-		return tipo;
-	}
-
-	bool Dato::EsString() const {
-		return !tipo;
-	}
-
-	bool Dato::MismoTipo(const Dato otro) const{
-		return (tipo == otro.tipo);
-	}
- 
- 	modulos::Nat Dato::ValorNat() const{
- 		return nat;
- 	}
-
- 	modulos::String Dato::ValorStr() const{
- 		return str;
- 	}
-
- 	bool Dato::MenorOIgual(const Dato otro) const{
- 		bool res;
- 			if(tipo){
-    		res = (nat <= otro.nat);
-			}else{
-			int i = 0;
-			while(i < str.size() && i < otro.str.size() && str[i] == otro.str[i]){
-				i++;
-			}
-			if(i == str.size() || i == otro.str.size()){
-			res = (str.size() <= otro.str.size() ); 
-			}else{
-			res = (str[i] <= otro.str[i] ) ;
-			}
-			}
-	}
-
-
- 	Dato Dato::MaxDato(const Conj<Dato> cd){
- 		typename Conj<Dato>::const_Iterador i = cd.CrearIt();
-		Dato res = i.Siguiente();
-		while(i.HaySiguiente()){
-			if(!(i.Siguiente().MenorOIgual(res))){
-				res = i.Siguiente();
-			}
-			i.Avanzar();
- 		}
- 	return res;
-	}
-
-	Dato Dato::MinDato(const Conj<Dato> cd){
- 		typename Conj<Dato>::const_Iterador i = cd.CrearIt();
-		Dato res = i.Siguiente();
-		while(i.HaySiguiente()){
-			if((i.Siguiente().MenorOIgual(res))){
-				res = i.Siguiente();
-			}
-			i.Avanzar();
- 		}
- 	return res;
-	}
-
 	ostream& operator<<(ostream& os, const Dato& d){
 		return d.mostrarDato(os);
 	}
-
-	ostream& Dato::mostrarDato(ostream& os) const
-	{
-		if (tipo) {
-			os << nat;
-		} else{
-			os << str;
-		}
-
-	return os;
-	}
-
-
-	 bool Dato::operator==(const Dato& otro) const
-	 {
-	 	bool res = false;
-	 	
-	 	if (tipo != otro.tipo){return res;}
-	 	
-	 	if (tipo)
-	 	{
-	 		res = (nat == otro.nat);
-	 	}
-	 	else
-	 	{
-	 		res = (str == otro.str);
-	 	}
-	 	return res;
-	 }
-
-	 bool Dato::operator!=(const Dato& otro) const 
-	 {
-	 	return !(operator==(otro));
-	 }
 
 };
 
